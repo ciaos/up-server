d408bdb6764aef76cf5642d3adccdb2616536d5e
root <root@vm6084.(none)>
2013-10-23 06:58:21 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Up-Server
 =====================
-上传服务器端代码，支持断点查询，超大文件上传
+上传服务器端代码，支持断点查询，超大文件上传，多线程上传，多文件一次上传
 * * *
 
 使用方法
@@ -83,7 +83,35 @@ curl -F "action=upload" -F "Filedata=@big.file" -H "Range: bytes=204800-303599/1
 ...
 ```
 
-*   对读写分片信息加文件锁还可实现多线程上传
+*   支持同时上传多个文件（不可与分片上传同时使用）
+```
+curl -F "action=upload" -F "Filedata1=@nspclient.js" -F "Filedata2=@nspclient_test.js" -v "http://10.6.2.11/up.php"
+* About to connect() to 127.0.0.1 port 80 (#0)
+*   Trying 127.0.0.1... connected
+* Connected to 127.0.0.1 (127.0.0.1) port 80 (#0)
+> POST /up.php HTTP/1.1
+> User-Agent: curl/7.19.7 (x86_64-suse-linux-gnu) libcurl/7.19.7 OpenSSL/0.9.8j zlib/1.2.3 libidn/1.10
+> Host: 127.0.0.1
+> Accept: */*
+> Content-Length: 13402
+> Expect: 100-continue
+> Content-Type: multipart/form-data; boundary=----------------------------8ed05e6400ca
+>
+< HTTP/1.1 100 Continue
+< HTTP/1.1 201 Created
+< Server: ngx_openresty
+< Date: Wed, 23 Oct 2013 14:54:31 GMT
+< Content-Type: text/html
+< Transfer-Encoding: chunked
+< Connection: keep-alive
+<
+* Connection #0 to host 127.0.0.1 left intact
+* Closing connection #0
+Upload OK
+
+```
+
+*   由于服务器端文件一旦写入，不会更改，所以支持多线程上传（由于未加锁，存在重复上传某分片的问题）
 
 Weibo Account
 -------------
--- up.php
+++ up.php
@@ -81,48 +81,48 @@ function lookupRange($rangefile){
     if(file_exists($rangefile)){
         $data = json_decode(file_get_contents($rangefile));
     }else{
-    	return array("Not Found","","");
+        return array("Not Found","","");
     }
     if(is_null($data) or is_null($data->range) or count($data->range) == 0) {
-    	return array("Not Found","","");
+        return array("Not Found","","");
     } else {
         $range = array_sort($data->range);
         $c = count($range);
-	if($c == 1 && $range[0][0] == 0 && $range[0][1]+1 == $data->totalsize){
-		return array("Created","","");
-	}
-	$start = 0;
-	$end = $data->totalsize-1;
+    if($c == 1 && $range[0][0] == 0 && $range[0][1]+1 == $data->totalsize){
+        return array("Created","","");
+    }
+    $start = 0;
+    $end = $data->totalsize-1;
         for($i=0;$i<$c;$i++){
-		if($range[$i][0]<=$start){
-			$start = $range[$i][1];
-			if($i+1<$c){
-				$end = $range[$i+1][0]-1;
-			}
-		} else{
-			$end = $range[$i][0]-1;
-		}
-		break;
-	}
-	return array($start,$end,$data->totalsize);
+        if($range[$i][0]<=$start){
+            $start = $range[$i][1];
+            if($i+1<$c){
+                $end = $range[$i+1][0]-1;
+            }
+        } else{
+            $end = $range[$i][0]-1;
+        }
+        break;
+    }
+    return array($start,$end,$data->totalsize);
     }
 }
 
 if(!is_null(@$_SERVER['REQUEST_METHOD']) && @$_SERVER['REQUEST_METHOD'] == "HEAD"){
     if(is_null($_SERVER["HTTP_FILENAME"])){
         header("HTTP/1.0 400 Bad Request");
-	exit(-1);
+    exit(-1);
     }
     $filename = $_SERVER["HTTP_FILENAME"];
     list($start,$end,$size) = lookupRange(rtrim($_SERVER['DOCUMENT_ROOT'] . $targetFolder,'/')."/".$filename.".range");
 
     if("Not Found" === $start){
-       	header("HTTP/1.0 404 File Not Found");
+           header("HTTP/1.0 404 File Not Found");
     } else if("Created" === $start){
-       	header("HTTP/1.0 201 Created");
+           header("HTTP/1.0 201 Created");
     } else{ 
-   	header("HTTP/1.0 202 Accepted");
-   	header("Range: bytes=$start-$end/$size");
+       header("HTTP/1.0 202 Accepted");
+       header("Range: bytes=$start-$end/$size");
     }
     exit(-1);
 }
@@ -132,52 +132,57 @@ if(!is_null(@$_SERVER['REQUEST_METHOD']) && @$_SERVER['REQUEST_METHOD'] != "POST
     exit(-1);
 }
 
-if (!empty($_FILES)) {
-    $tempFile = $_FILES['Filedata']['tmp_name'];
-    $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
-    $targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
-    
-    if(is_null($_SERVER["HTTP_RANGE"])){
-        move_uploaded_file($tempFile,iconv("UTF-8","gb2312", $targetFile));
-	$size = $_FILES['Filedata']['size'];
-	updateRange($targetFile.".range",0,$size-1,$size);
-        header("HTTP/1.0 201 Created");
-        echo "Upload OK";
-    } else {
-        $pos = $_SERVER["HTTP_RANGE"];
-        $range = ltrim($pos, "bytes=");
-        list($start,$end,$size) = split('[-/]',$range);
-	$pos = $start;
-        $r_fp = fopen($tempFile, 'r');
-        if(file_exists($targetFile)){
-            $w_fp = fopen($targetFile, 'r+');
-        }else{
-            $w_fp = fopen($targetFile, 'w+');
-        }
-        while($data = fread($r_fp, 4092)){
-            flock($w_fp,LOCK_EX);
-            fseek($w_fp, $pos, SEEK_SET);
-            fwrite($w_fp, $data);
-            flock($w_fp,LOCK_UN);
-            $pos += strlen($data);
-        }
-
-        fclose($r_fp);
-        fclose($w_fp);
-	updateRange($targetFile.".range",$start,$end,$size);
-	list($start,$end,$size) = lookupRange($targetFile.".range");
-
-        if("Not Found" === $start){
-            header("HTTP/1.0 500 Internal Server Error");
-            echo "Upload Failed";
-        } else if("Created" === $start){
+$response = "";
+if (!empty($_FILES) && is_array($_FILES)) {
+    foreach($_FILES as $k => $v){
+        $tempFile = $v['tmp_name'];
+        $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+        $targetFile = rtrim($targetPath,'/') . '/' . $v['name'];
+        
+        if(is_null($_SERVER["HTTP_RANGE"])){
+            move_uploaded_file($tempFile,iconv("UTF-8","gb2312", $targetFile));
+            $size = $v['size'];
+            updateRange($targetFile.".range",0,$size-1,$size);
             header("HTTP/1.0 201 Created");
-            echo "Upload OK";
+            $response = "Upload OK";
         } else {
-            header("HTTP/1.0 202 Accepted");
-	    header("Range: bytes=$start-$end/$size");
-            echo "Upload Continue";
+            $pos = $_SERVER["HTTP_RANGE"];
+            $range = ltrim($pos, "bytes=");
+            list($start,$end,$size) = split('[-/]',$range);
+            $pos = $start;
+            $r_fp = fopen($tempFile, 'r');
+            if(file_exists($targetFile)){
+                $w_fp = fopen($targetFile, 'r+');
+            }else{
+                $w_fp = fopen($targetFile, 'w+');
+            }
+            while($data = fread($r_fp, 4092)){
+                flock($w_fp,LOCK_EX);
+                fseek($w_fp, $pos, SEEK_SET);
+                fwrite($w_fp, $data);
+                flock($w_fp,LOCK_UN);
+                $pos += strlen($data);
+            }
+    
+            fclose($r_fp);
+            fclose($w_fp);
+            updateRange($targetFile.".range",$start,$end,$size);
+            list($start,$end,$size) = lookupRange($targetFile.".range");
+    
+            if("Not Found" === $start){
+                header("HTTP/1.0 500 Internal Server Error");
+                echo "Upload Failed";
+                exit(-1);
+            } else if("Created" === $start){
+                header("HTTP/1.0 201 Created");
+                $response = "Upload OK";
+            } else {
+                header("HTTP/1.0 202 Accepted");
+                header("Range: bytes=$start-$end/$size");
+                $response = "Upload Continue";
+            }
         }
     }
+    echo $response;
 }
 ?>
